05f95dc9a2dd8fe4cd779e2410259a7069844abb
AleksandrKlesh <alex73kl@gmail.com>
2024-01-23 12:10:39 +0700

    refactor: change path to file using __dirname global object

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
+const path = require("node:path")
 const { stdout } = process;
 
-const readableStream = fs.createReadStream("./01-read-file/text.txt", "utf-8");
+const readableStream = fs.createReadStream(`${__dirname}/text.txt`, "utf-8");
 readableStream.on("data", (chunk) => stdout.write(chunk));
