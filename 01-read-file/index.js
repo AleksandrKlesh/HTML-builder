2f6947ec048e180677047e4e3d911983f35505c4
AleksandrKlesh <alex73kl@gmail.com>
2025-01-20 21:49:26 +0700

    feat: implement a script that outputs the contents of the file text.txt to the console

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const file = path.join(`${__dirname}`, 'text.txt');
+const readableStream = fs.createReadStream(file, 'utf-8');
+
+readableStream.on('data', (chunk) => console.log(chunk));
